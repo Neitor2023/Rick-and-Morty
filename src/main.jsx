510b13b7a8716385d70312e1743d5ee09853f787
Romy Garcia <neitorme@gmail.com>
2023-03-13 10:34:12 -0500

    Montando Loader en proceso

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
 // import Loader from './components/loader'
+console.log("Modulo Scrip")
 
 window.addEventListener('load', () => {
+    console.log("dentro de addEventListener")
     const container_loader = document.querySelector('.container_loader')
     container_loader.style.opacity = 0
     container_loader.style.visibility = 'hidden'
 })
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />,
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App />,
+// );
